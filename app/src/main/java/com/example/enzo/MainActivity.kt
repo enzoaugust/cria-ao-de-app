8ac5035539495d91f1d9bab1323202614c016e9c
enzoaugust <enzoaugustosoares06@gmail.com>
2023-04-18 11:37:35 -0300

    convite

--- app/src/main/java/com/example/enzo/MainActivity.kt
+++ app/src/main/java/com/example/enzo/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.enzo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.enzo.ui.theme.EnzoTheme
 
@@ -20,9 +25,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.Blue
                 ) {
-                    Greeting("Enzo Criation")
+                    PlanoDeFundo()
                 }
             }
         }
@@ -30,14 +35,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun PlanoDeFundo(){
+    Image(
+        painter = painterResource(id = R.drawable.androidparty),
+        contentDescription = null,
+        contentScale = ContentScale.Crop
 
+    )
+Cabecalho(
+    nomeCompleto = "Enzo Augusto",
+    cargo = "aluno"
+)
+
+}
+@Composable
+fun Cabecalho(nomeCompleto:String,cargo:String,modifier:Modifier=Modifier) {
+
+
+    Column() {
+        Text(
+            text = nomeCompleto,
+            modifier = modifier
+        )
+        Text(
+            text = cargo,
+            modifier = modifier
+        )
+    }
+
+
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     EnzoTheme {
-        Greeting("Android")
+        PlanoDeFundo()
     }
 }
